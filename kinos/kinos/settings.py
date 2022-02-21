dcb5ecfbb4bcab2b7a2ec031696c17681d2c9783
KeplerDE <90717368+KeplerDE@users.noreply.github.com>
2022-02-21 17:18:12 +0300

    Filters ?year

--- kinos/kinos/settings.py
+++ kinos/kinos/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'movies',
+    'django_filters',
 
 ]
 
@@ -216,5 +217,10 @@ CKEDITOR_CONFIGS = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 
--- kinos/movies/views.py
+++ kinos/movies/views.py
@@ -1,7 +1,6 @@
 from django.db import models
 from rest_framework import generics
-
-
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Movie, Actor
 from .serializers import (
     MovieListSerializer,
@@ -11,11 +10,14 @@ from .serializers import (
     ActorListSerializer,
     ActorDetailSerializer,
 )
-from .service import get_client_ip
+from .service import get_client_ip, MovieFilter
+
 
 class MovieListView(generics.ListAPIView):
     """Вывод списка фильмов"""
     serializer_class = MovieListSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = MovieFilter
 
     def get_queryset(self):
         movies = Movie.objects.filter(draft=False).annotate(
@@ -25,20 +27,17 @@ class MovieListView(generics.ListAPIView):
             middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
         )
         return movies
-#переделали на метод get_queryset
+
 
 class MovieDetailView(generics.RetrieveAPIView):
     """Вывод фильма"""
     queryset = Movie.objects.filter(draft=False)
     serializer_class = MovieDetailSerializer
 
-# также переделали
 
 class ReviewCreateView(generics.CreateAPIView):
     """Добавление отзыва к фильму"""
     serializer_class = ReviewCreateSerializer
-
-#переписали указали класс сериализации
 
 
 class AddStarRatingView(generics.CreateAPIView):
@@ -49,9 +48,7 @@ class AddStarRatingView(generics.CreateAPIView):
         serializer.save(ip=get_client_ip(self.request))
 
 
-#такая же история только созраняем ай пи адрес и селф мы принимаем через request
-
-class ActorsListView(generics.ListAPIView):   #наследуемся от джнерика
+class ActorsListView(generics.ListAPIView):
     """Вывод списка актеров"""
     queryset = Actor.objects.all()
     serializer_class = ActorListSerializer
